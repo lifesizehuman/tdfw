1e0946fe5d8451d8d6880303c8cfb2c4ebcca9df
Slater Combes <combes.slater@gmail.com>
2018-04-13 11:54:54 -0700

    map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+const firebase = require('firebase');
 
 @Component({
-  selector: "app-map",
-  templateUrl: "./map.component.html",
-  styleUrls: ["./map.component.css"]
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
   title: string;
@@ -11,9 +12,28 @@ export class MapComponent implements OnInit {
   lng: number;
 
   constructor() {
-    this.title = "string";
+    this.title = 'string';
     this.lat = 51.678418;
     this.lng = 7.809007;
-  } 
-  ngOnInit () {}
+  }
+
+  ngOnInit () {
+
+    const config = {
+      apiKey: 'AIzaSyAqMI1aab7fpTQsdo7zUXnXAhnIwVTdBAs',
+      authDomain: 'turn-down-for-whales.firebaseapp.com',
+      databaseURL: 'https://turn-down-for-whales.firebaseio.com',
+      projectId: 'turn-down-for-whales',
+      storageBucket: 'turn-down-for-whales.appspot.com',
+      messagingSenderId: '612763934802'
+    };
+    firebase.initializeApp(config);
+
+    const database = firebase.database();
+
+    database.ref('/sightings').limitToLast(5).on('child_added', function(childSnapshot, prevChildKey) {
+
+        console.log(childSnapshot.val());
+    });
+  }
 }
